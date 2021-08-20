9464c688ffcf84f1b0123def3bc81750bd7a53c8
Devzila <nilay@devzila.com>
2021-08-20 12:33:14 +0530

    Create build.php

---
+++ build.php
@@ -0,0 +1,24 @@
+<html>
+  <head><title>Message</title></head>
+  <body>
+    
+<?php
+    
+  function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    print("<pre>");
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+    print("</pre>");
+  }
+    
+  // Print the exec output inside of a pre element
+  execPrint("cd /var/www/bpo-project-web");  
+  execPrint("git pull origin main");
+  execPrint("git status");  
+    
+?>
+  </body>
+</html>  
